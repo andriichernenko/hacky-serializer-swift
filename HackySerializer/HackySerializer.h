212a608eb126f131b8816ed8922cc94b655714e7
Andrii Chernenko <mail@andrii.ch>
2016-03-16 01:01:13 +0100

    Initial Commit

---
+++ HackySerializer/HackySerializer.h
@@ -0,0 +1,19 @@
+//
+//  HackySerializer.h
+//  HackySerializer
+//
+//  Created by Andrii Chernenko on 16/03/16.
+//  Copyright © 2016 Andrii Chernenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HackySerializer.
+FOUNDATION_EXPORT double HackySerializerVersionNumber;
+
+//! Project version string for HackySerializer.
+FOUNDATION_EXPORT const unsigned char HackySerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HackySerializer/PublicHeader.h>
+
+
